6460cac5023115832c7a1bade2a9403c50bcffca
tqc-michalp <michal.pawlak@thinqcraft.com>
2019-06-13 21:10:48 +0200

    boilerplate base component

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React, {ReactComponentElement} from 'react';
+import ReactDom from 'react-dom';
+
+class Base extend React.Component {
+
+    render(){
+        return(
+            <h2>test</h2>
+        )
+    }
+}
