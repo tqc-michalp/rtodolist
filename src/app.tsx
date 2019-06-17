2863e9efb286124d3201a3fc434c3d0c98fd5e5a
tqc-michalp <michal.pawlak@thinqcraft.com>
2019-06-17 21:28:43 +0200

    configured webpack and TS

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,23 @@ import * as React from 'react';
 import * as ReactDom from 'react-dom';
 
 class Base extends React.Component<{}> {
+    // const list = [
+    //     {a: 1,
+    //     b: 2
+    //     }
+    // ]
     render() {
         return(
-            <h2>ToDoList on React</h2>
-            <p>Lets burn sth!</p>
-        )
+            <>
+                <h2>ToDoList on React</h2>
+                <p>test</p>
+                {/*<ul>*/}
+                {/*list.map( e =>  <li>{e.name}</li> )*/}
+                {/*</ul>*/}
+            </>
+        );
     }
 }
 
-ReactDom.render("entry-point", <Base />)
+ReactDom.render(<Base />, document.getElementById('entry-point'));
+// ReactDom.render('#entry-point', <Base />);
