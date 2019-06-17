55242bdeb7204671741ea5986b5ee950f64fc4fb
exaesel <exaesel@gmail.com>
2019-06-17 21:28:43 +0200

    configured webpack and TS

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/* eslint-env node */
+
+'use strict';
+
+const path = require('path');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                include: [
+                    path.resolve(__dirname, 'src')
+                ],
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new copyWebpackPlugin([
+            { from: 'src/index.html', to: 'index.html' }
+        ])
+    ]
+};
+
